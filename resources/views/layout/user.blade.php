543576cbeb1ea9986a3258582dc6957bd9355054
hongthai <thai2bake20157@gmail.com>
2025-07-10 04:09:00 +0700

    sua ok phan javascript

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -186,24 +186,7 @@
     </footer>
     <!-- End footer Area -->
 
-    <script src="{{asset('user')}}/js/vendor/jquery-2.2.4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.{{asset('user')}}/js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-        crossorigin="anonymous"></script>
-    <script src="{{asset('user')}}/js/vendor/bootstrap.min.js"></script>
-    <script src="{{asset('user')}}/js/jquery.ajaxchimp.min.js"></script>
-    <script src="{{asset('user')}}/js/jquery.nice-select.min.js"></script>
-    <script src="{{asset('user')}}/js/jquery.sticky.js"></script>
-    <script src="{{asset('user')}}/js/nouislider.min.js"></script>
-    <script src="{{asset('user')}}/js/countdown.js"></script>
-    <script src="{{asset('user')}}/js/jquery.magnific-popup.min.js"></script>
-    <script src="{{asset('user')}}/js/owl.carousel.min.js"></script>
-    <!--gmaps Js-->
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
-    <script src="{{asset('user')}}/js/gmaps.min.js"></script>
-    <script src="{{asset('user')}}/js/main.js"></script>
-    <script>
-        const ASSET_URL = "{{asset('user')}}"
-    </script>
+    @yield('scripts')
 </body>
 
 </html>
