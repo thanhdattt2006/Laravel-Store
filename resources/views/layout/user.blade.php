80b182aa88d8fe8290fd9dc7529d457d5c8b2a9e
hongthai <thai2bake20157@gmail.com>
2025-07-10 05:51:59 +0700

    sua ok thanh navbar

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -83,7 +83,6 @@
                             </li>
                             <li class="nav-item"><a class="nav-link" href="{{url('contact/index')}}">Contact</a></li>
 
-                            <!-- hsdfsdfds -->
                              <li class="nav-item submenu dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                                     aria-expanded="false">
@@ -91,8 +90,8 @@
                                 </a>
                                 <ul class="dropdown-menu">
                                     <li class="nav-item"><a class="nav-link" href="{{url('/login')}}">Log-in</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="{{url('admin/confirmation')}}">Confirmation</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="{{url('admin/tracking')}}">Tracking</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="{{url('shop/confirmation')}}">Confirmation</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="{{url('/tracking')}}">Tracking</a></li>
                                     <li class="nav-item"><a class="nav-link" href="#">
                                         <span>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(255, 186, 0, 1);"><path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path><path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path></svg>
