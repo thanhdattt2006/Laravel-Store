d9b17f23bb8683f9493988b5dbf824b5a7b00628
thtoai1996 <nood071996@gmail.com>
2025-07-21 10:35:11 +0700

    pull

--- resources/views/shop/wishlistProduct.blade.php
+++ resources/views/shop/wishlistProduct.blade.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
             <div class="col-first">
-                <h1>Compare Products Page</h1>
+                <h1>Wishlist Products Page</h1>
                 <nav class="d-flex align-items-center">
                     <a href="index.html">Home<span class="lnr lnr-arrow-right"></span></a>
                     <a href="#">Shop<span class="lnr lnr-arrow-right"></span></a>
