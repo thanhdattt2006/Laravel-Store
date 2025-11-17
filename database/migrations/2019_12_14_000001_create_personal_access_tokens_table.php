4bb31cd174f4109ec83bed3f3545f6cda57bc889
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-11-17 20:59:43 +0700

    Optimize: Merge add_remember_token into create_account_table migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -21,6 +20,7 @@ return new class extends Migration
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
