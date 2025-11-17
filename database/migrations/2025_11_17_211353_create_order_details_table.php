e47eb0a3ba47ed4f262645bc5cbe26844b6194e1
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-11-17 22:09:13 +0700

    ...

--- database/migrations/2025_11_17_211353_create_order_details_table.php
+++ database/migrations/2025_11_17_211353_create_order_details_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
         Schema::create('order_details', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained('product');
-            $table->foreignId('order_id')->constrained('order');
+            $table->foreignId('order_id')->constrained('orders');
             $table->integer('quantity');
             $table->integer('price');
             $table->integer('total_price');
