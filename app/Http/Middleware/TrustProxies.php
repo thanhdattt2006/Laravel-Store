996ac6aaeebe431fbd7b1e692633065ef3cd853d
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-11-17 23:18:17 +0700

    roll back proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request; // <--- DÒNG NÀY RẤT QUAN TRỌNG
+use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
@@ -12,12 +12,17 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*'; // Đã sửa
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL; // Đã sửa
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
