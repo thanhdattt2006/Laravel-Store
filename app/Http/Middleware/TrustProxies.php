42afb0635aebf35e81a932bb423c65811e5dab89
thanhdattt2006 <thanhdattt2006@gmail.com>
2025-11-17 23:38:00 +0700

    ...

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,10 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; // <--- DÒNG NÀY PHẢI CÓ
 
 class TrustProxies extends Middleware
 {
-    protected $proxies = '*';
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies = '*'; // <--- CHẮC CHẮN CÓ DÒNG NÀY
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL; // <--- DÒNG NÀY PHẢI ĐÚNG
 }
